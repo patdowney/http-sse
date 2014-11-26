b9c7a2fc17c56f97ba575a83e493245cdb08753b
Pat Downey <pat.downey@gmail.com>
2014-11-26 23:49:22 +0000

    add example of handlin ctrl-c

--- examples/http-example/example.go
+++ examples/http-example/example.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/patdowney/http-sse"
@@ -20,9 +22,20 @@ func (e ExampleEvent) Event() string { return e.event }
 func (e ExampleEvent) Data() string  { return e.data }
 
 func main() {
-	b := sse.NewBroker()
-	b.Start()
+	b := sse.StartNewBroker()
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			b.Stop()
+			// sig is a ^C, handle it
+			time.Sleep(100 * time.Millisecond)
+			os.Exit(0)
+		}
+	}()
+
+	// generate some events
 	go func() {
 		c := time.Tick(200 * time.Millisecond)
 		id := 0
@@ -33,15 +46,37 @@ func main() {
 			id += 1
 		}
 	}()
+	/*
+		// shutdown the broker if told to
+		go func() {
+			select {
+			case <-time.After(30 * time.Second):
+				b.Stop()
+			}
+		}()
 
+		// start the broker again  for kicks
+		go func() {
+			select {
+			case <-time.After(35 * time.Second):
+				b.Start()
+			}
+		}()
+	*/
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		s, _ := sse.NewEventStream(w)
-		s.Start()
+		s, err := sse.StartNewEventStream(w)
+		if err != nil {
+			log.Printf("error-starting-stream: %s", err.Error())
+			return
+		}
 		b.Subscribe(s)
 
-		select {
-		case <-s.CloseNotify():
-			b.Unsubscribe(s)
+		for {
+			select {
+			case <-s.CloseNotify():
+				b.Unsubscribe(s)
+				return
+			}
 		}
 	})
 
